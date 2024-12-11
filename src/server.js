fd22d908ddc9575d3941392b95061a77d7fa49cb
its_kcradheshyam <kcradheshyam08@gmail.com>
2024-12-12 02:02:11 +0545

    add json parse

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ dotenv.config('.env');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('*', (req, res) => {
     res.status(404).send('Page not found');
